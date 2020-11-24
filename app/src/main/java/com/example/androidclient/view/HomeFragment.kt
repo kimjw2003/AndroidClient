99a938598ee25c22e03aee00784f59dad26acfaa
LMY <lmun768@naver.com>
2020-11-24 12:46:41 +0900

    [UPDATE] Main

--- app/src/main/java/com/example/androidclient/view/HomeFragment.kt
+++ app/src/main/java/com/example/androidclient/view/HomeFragment.kt
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.androidclient.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
 
 class HomeFragment : Fragment() {
 
@@ -20,5 +22,19 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setDate()
+    }
+
+    fun setDate()
+    {
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR).toShort()
+        val month = (cal.get(Calendar.MONTH) + 1).toShort()
+        val day = cal.get(Calendar.DATE).toShort()
+        room_list_date.text = ("${year}년 ${month}월 ${day}일")
     }
 }
--- app/src/main/java/com/example/androidclient/view/RoomListActivity.kt
+++ app/src/main/java/com/example/androidclient/view/RoomListActivity.kt
@@ -3,10 +3,14 @@ package com.example.androidclient.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidclient.R
+import kotlinx.android.synthetic.main.activity_room_list.*
+import java.util.*
 
 class RoomListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_list)
     }
+
+
 }
