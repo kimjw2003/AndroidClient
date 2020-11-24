88da06503977f516d240df8fd1fec17695b23a75
LMY <lmun768@naver.com>
2020-11-24 16:11:29 +0900

    [UPDATE] Make_team

--- app/src/main/java/com/example/androidclient/adapter/MakeTeamAdapter.kt
+++ app/src/main/java/com/example/androidclient/adapter/MakeTeamAdapter.kt
@@ -1,24 +1,16 @@
 package com.example.androidclient.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidclient.R
 import com.example.androidclient.data.MakeTeamData
 import com.example.androidclient.data.MyRoomData
 
 class MakeTeamAdapter(val makeTeamData: ArrayList<MakeTeamData>) : RecyclerView.Adapter<MakeTeamAdapter.Holder>() {
-
-    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
-        fun bind(data : MakeTeamData){
-
-            itemView.setOnClickListener {
-
-            }
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.make_team_item, parent, false)
@@ -31,5 +23,15 @@ class MakeTeamAdapter(val makeTeamData: ArrayList<MakeTeamData>) : RecyclerView.
 
     override fun getItemCount(): Int {
         return makeTeamData.size
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val name = itemView.findViewById<TextView>(R.id.item_team_people_name)
+
+        fun bind(data : MakeTeamData){
+            Log.d("TAG", data.teamMateName)
+            name.text = data.teamMateName
+        }
     }
 }
--- app/src/main/java/com/example/androidclient/view/MakeTeamFragment.kt
+++ app/src/main/java/com/example/androidclient/view/MakeTeamFragment.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.androidclient.R
+import com.example.androidclient.adapter.MakeTeamAdapter
+import com.example.androidclient.adapter.RoomListAdapter
+import com.example.androidclient.data.MakeTeamData
+import com.example.androidclient.data.response.RoomResponse
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_maketeam.*
+import java.util.ArrayList
 
 class MakeTeamFragment : Fragment() {
+
+    var teamList : ArrayList<MakeTeamData> = arrayListOf()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -17,5 +27,23 @@ class MakeTeamFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_maketeam, container, false)
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        team_make_add_team.setOnClickListener {
+            if(team_make_people_name.text.isEmpty())
+            {
+                Toast.makeText(context, "팀원 이름을 넣어주세요", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                teamList.add(MakeTeamData(team_make_people_name.text.toString()))
+                team_make_people_name.setText("")
+                val mAdapter = MakeTeamAdapter(teamList)
+                make_team_recyclerview.setHasFixedSize(true)
+                make_team_recyclerview.adapter = mAdapter
+            }
+        }
     }
 }
