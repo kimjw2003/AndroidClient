2ad6832082ffbd24302864f1b3b4b682e02a0831
kimjw2003 <kimjw111003@gmail.com>
2020-11-24 14:33:30 +0900

    [UPDATE] myRoom, myTeam

--- app/src/main/java/com/example/androidclient/view/MyRoomFragment.kt
+++ app/src/main/java/com/example/androidclient/view/MyRoomFragment.kt
@@ -1,10 +1,14 @@
 package com.example.androidclient.view
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidclient.R
 import com.example.androidclient.adapter.MyRoomAdapter
@@ -13,17 +17,27 @@ import kotlinx.android.synthetic.main.fragment_myroom.*
 import kotlinx.android.synthetic.main.fragment_myroom.view.*
 
 class MyRoomFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_myroom, container, false)
 
         val myRoomList = ArrayList<MyRoomData>()
             myRoomList.add(MyRoomData("room1", "예약가능"))
 
-        val adapter = MyRoomAdapter(requireContext(),myRoomList)
+        val adapter = MyRoomAdapter(requireContext(), myRoomList)
         view.myRoomRcView.adapter = adapter
 
-
+        view.team_Btn.setOnClickListener {
+             setFrag(0)
+        }
         return view
     }
-
+    private fun setFrag(fragNum : Int) {
+        val ft = childFragmentManager.beginTransaction() //화면 교체를 위한 트랜잭션
+        when(fragNum){
+            0 -> {
+                ft.replace(R.id.myRoom_Frame, MyTeamFragment()).commit()
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/androidclient/view/MyTeamFragment.kt
+++ app/src/main/java/com/example/androidclient/view/MyTeamFragment.kt
@@ -6,16 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.androidclient.R
+import kotlinx.android.synthetic.main.fragment_myteam.*
+import kotlinx.android.synthetic.main.fragment_myteam.view.*
 
 class MyTeamFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_myteam, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_myteam, container, false)
 
-        return root
+        view.room_Btn.setOnClickListener {
+            setFrag(0)
+        }
+
+        return view
+    }
+
+    private fun setFrag(fragNum : Int) {
+        val ft = childFragmentManager.beginTransaction() //화면 교체를 위한 트랜잭션
+        when(fragNum){
+            0 -> {
+                ft.replace(R.id.myTeam_Frame, MyRoomFragment()).commit()
+            }
+        }
     }
 }
