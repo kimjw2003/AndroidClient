a74cac87357e164fb73f9fa62b1acb1f6e62450c
structsize <52183019+structsize@users.noreply.github.com>
2020-11-24 11:36:51 +0900

    [UPDATE] RoomInfo xml

--- app/src/main/java/com/example/androidclient/view/RoomInfoActivity.kt
+++ app/src/main/java/com/example/androidclient/view/RoomInfoActivity.kt
@@ -2,11 +2,20 @@ package com.example.androidclient.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.androidclient.R
+import kotlinx.android.synthetic.main.activity_room_info.*
 
 class RoomInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_info)
+
+        var teamList = ArrayList<String>()
+
+        teamList.add("팀 명")
+        // 서버에서 팀 리스트 얻어오기
+
+        spinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, teamList)
     }
 }
